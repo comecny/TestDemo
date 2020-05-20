0dadd9644ff083e1f66cb92767273622e4d02b93
liuxuchen0-0 <814127107@qq.com>
2020-05-20 22:19:27 +0800

    6

--- bulid.sh
+++ bulid.sh
@@ -1,9 +1,8 @@
-mvn clean
-
-mvn package -DskipTests
-
-docker rmi -f lgj/webdemo:1.0
-
-mvn dockerfile:build
-
-docker images
+MAVEN_HOME=/usr/local/maven
+PATH=$PATH:$MAVEN_HOME/bin
+mvn clean package docker:build
+echo "当前docker 镜像："
+docker images | grep dockerspringboot
+echo "启动容器----->"
+docker run -p 8001:8001 -d dockerspringboot
+echo "启动服务成功！"
--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -3,11 +3,16 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/hello")
-    public String hello(){
-        return "hello world";
+    public Map hello(){
+        Map hashMap = new HashMap();
+        hashMap.put("hello","hello wold");
+        return hashMap;
     }
 }
