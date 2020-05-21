41f9d27d63641e7441125911cb391f215fc85134
liuxuchen0-0 <814127107@qq.com>
2020-05-21 08:20:01 +0800

    提交

--- bulid.sh
+++ bulid.sh
@@ -1,8 +1,21 @@
+# 引导MAVEN_HOME
 MAVEN_HOME=/usr/local/maven
 PATH=$PATH:$MAVEN_HOME/bin
-mvn clean package docker:build
-echo "当前docker 镜像："
+# 先删除之前的容器
+echo "remobe old container"
+docker ps -a | grep dockerspringboot | awk '{print $1}'| xargs docker rm -f
+# 删除之前的镜像
+echo "romove old image"
+docker rmi dockerspringboot
+# 构建镜像
+mvn docker:build
+# 打印当前镜像
+echo "current docker images"
 docker images | grep dockerspringboot
-echo "启动容器----->"
+# 启动容器
+echo "start container"
 docker run -p 8001:8001 -d dockerspringboot
-echo "启动服务成功！"
+# 打印当前容器
+echo "current container"
+docker ps -a | grep dockerspringboot
+echo "star service success!"
